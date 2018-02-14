cfac48994c0c5fe18b79aa8933b09e9fff8c78c5
Doug Chestnut <dougchestnut@gmail.com>
2018-02-13 23:22:20 -0500

    added libcal feed parser and some gitignore rules

---
+++ libcal-Hours/index.js
@@ -0,0 +1,44 @@
+exports.handler = (event, context, callback) => {
+    var https = require('https');
+    var values = require('object.values');
+
+    // take the array of weeks and turn it into a hash of days
+    var toDays = function(weeks){
+      if(Array.isArray(weeks) && weeks.length>0) {
+        var days = {};
+        weeks.forEach((w)=>{
+          var dayHash = {};
+          if (!Object.values) {
+            values.shim();
+          }
+          Object.values(w).forEach(function(day){
+            if (day.times.hours && day.rendered)
+              dayHash[day.date] = {hours:day.times.hours, rendered: day.rendered};
+          });
+          Object.assign(days, dayHash);
+        });
+        return days;
+      } else {
+        return null;
+      }
+    }
+
+    // LibCal API call
+    https.get('https://api3.libcal.com/api_hours_grid.php?iid=863&format=json&weeks=52',res=>{
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", data => {
+        body+=data;
+      });
+      res.on("end", ()=>{
+        var json = JSON.parse(body).locations;
+        var hash = {};
+        json.forEach(function(loc){
+          hash[loc.lid] = toDays(loc.weeks);
+        });
+        //console.log(hash);
+        callback(null, hash);
+      });
+    });
+
+};
