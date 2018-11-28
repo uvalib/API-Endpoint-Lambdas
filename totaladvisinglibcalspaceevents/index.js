9cf027f86ae1ae7eb8f6917dcf5c8652e4c72a42
jlk4p <jlk4p@virginia.edu>
2018-11-28 15:47:05 -0500

    put space location IDs into the API as mapped value

--- totaladvisinglibcalspaceevents/index.js
+++ totaladvisinglibcalspaceevents/index.js
@@ -21,47 +21,47 @@ exports.handler = (event, context, callback) => {
         // Make Spaces API call for desired location Spaces
         // 3780 and 3781 are the Georges Student Center study room and conferenece room groups
         request({
-            url: 'https://api2.libcal.com/1.1/space/nickname/3780,3781',
+            url: 'https://api2.libcal.com/1.1/space/nickname/' + event['query']['spaceCategories'],
             method: 'GET',
             auth: {
-              'bearer': access_token
+                'bearer': access_token
             }
         }, function(err, res) {
-          if (err) return callback(err);
-          var data = JSON.parse(res.body);
-          var categories = data[0].categories;
-          // loop through categories, aka grouped spaces
-          for (var i=0; i<categories.length; i++) {
-            var grp_name = categories[i].name.substring(0, categories[i].name.length-1);
-            // loop through spaces to get events and generate ics file content for each
-            for (var j=0; j<categories[i].spaces.length; j++) {
-              var room_num = categories[i].spaces[j].name.replace(/\D/g, '');
-              var location = categories[i].spaces[j].name;
-              var room_name = location + ' ' + grp_name;
-              var ics_file = ical({
-                name: room_name,
-                prodId: {company: company_name, product: product_name},
-                timezone: 'America/New_York',
-                method: 'publish'
-              });
-              ics_file.ttl(60 * 30); //time to live of 30 minutes (same as API cache and Springshare)
-              categories[i].spaces[j].bookings.forEach(function(evt, index, init_array) {
-                ics_file.createEvent({summary: evt.nickname, start: evt.start, end: evt.end, timestamp: evt.created, location: room_name});
-              });
-              //console.log(ics_file.toString());
-              space_ics_files.push({room: room_num, data: ics_file.toString()});
-            } // for j
-          } // for i
-          // if the API call passes the room parameter, then use the code below to
-          // retreive the desired ics data to return.
-          //space_ics_files.forEach(function(space, index, init_array) {
-          //  if (space.room == event.room) {
-          //    callback(space.data, 'Success');
-          //  }
-          //});
-          // callback should return the space_ics_files dataset and then the API call using this
-          // Lambda function should retrieve the appropriate room data and return it.
-          callback(null, space_ics_files);
+            if (err) return callback(err);
+            var data = JSON.parse(res.body);
+            var categories = data[0].categories;
+            // loop through categories, aka grouped spaces
+            for (var i = 0; i < categories.length; i++) {
+                var grp_name = categories[i].name.substring(0, categories[i].name.length - 1);
+                // loop through spaces to get events and generate ics file content for each
+                for (var j = 0; j < categories[i].spaces.length; j++) {
+                    var room_num = categories[i].spaces[j].name.replace(/\D/g, '');
+                    var location = categories[i].spaces[j].name;
+                    var room_name = location + ' ' + grp_name;
+                    var ics_file = ical({
+                        name: room_name,
+                        prodId: { company: company_name, product: product_name },
+                        timezone: 'America/New_York',
+                        method: 'publish'
+                    });
+                    ics_file.ttl(60 * 30); //time to live of 30 minutes (same as API cache and Springshare)
+                    categories[i].spaces[j].bookings.forEach(function(evt, index, init_array) {
+                        ics_file.createEvent({ summary: evt.nickname, start: evt.start, end: evt.end, timestamp: evt.created, location: room_name });
+                    });
+                    //console.log(ics_file.toString());
+                    space_ics_files.push({ room: room_num, data: ics_file.toString() });
+                } // for j
+            } // for i
+            // if the API call passes the room parameter, then use the code below to
+            // retreive the desired ics data to return.
+            //space_ics_files.forEach(function(space, index, init_array) {
+            //  if (space.room == event.room) {
+            //    callback(space.data, 'Success');
+            //  }
+            //});
+            // callback should return the space_ics_files dataset and then the API call using this
+            // Lambda function should retrieve the appropriate room data and return it.
+            callback(null, space_ics_files);
         });
     });
 };
