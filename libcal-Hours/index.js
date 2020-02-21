8b9c0443e8fe23c9cc2c541caba5ccf5dc25b9de
Doug Chestnut <dougchestnut@gmail.com>
2020-02-21 08:27:57 -0500

    fix for hours api change - Again

--- libcal-Hours/index.js
+++ libcal-Hours/index.js
@@ -33,15 +33,15 @@ exports.handler = (event, context, callback) => {
         body+=data;
       });
       res.on("end", ()=>{
-        var json = JSON.parse(body); //.locations;
+        var json = JSON.parse(body).locations;
         var hash = {};
-        Object.keys(json).forEach((key)=>{
-          let loc = json[key];
-          hash[loc.lid] = toDays(loc.weeks);
-        })
-//        json.forEach(function(loc){
+//        Object.keys(json).forEach((key)=>{
+//          let loc = json[key];
 //          hash[loc.lid] = toDays(loc.weeks);
-//        });
+//        })
+        json.forEach(function(loc){
+          hash[loc.lid] = toDays(loc.weeks);
+        });
 //        console.log(hash)
         callback(null, hash);
       });
