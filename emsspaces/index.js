6880b7569503c88017ddddbb3d675cf19ecc9eec
Jack Kelly <jlk4p@virginia.edu>
2021-08-23 16:24:07 -0400

    update emsspaces based on API

--- emsspaces/index.js
+++ emsspaces/index.js
@@ -4,8 +4,14 @@ exports.handler = (event, context, callback) => {
         company_name = 'UVA Library',
         product_name = 'AWS Lambda',
         space_ics_files = new Array();
+    // ID | Room     | Description
+    //  6 | 318/318A | Harrison/Small 318
+    // 16 | CLEM407  | Clemons 407
+    // 17 | CLK133   | Brown 133
+    // 19 | CLEM322  | Clemons 322
+    var roomIdArray = [ 6, 16, 17, 19 ];
+
     // authentication for EMS API access
-    // @TODO ask Dave Goldstein how I go about setting environment variables for this.
     request({
         url: 'http://lb-ems.eservices.virginia.edu/EmsPlatform/api/v1/clientauthentication/ ',
         method: 'POST',
@@ -18,56 +24,60 @@ exports.handler = (event, context, callback) => {
         // Save client token for EMS API calls
         var json = JSON.parse(res.body);
         var clientToken = json.clientToken;
-        // Make EMSSpaces API call for desired location(s)
-        // @TODO All EMS Library rooms are pulled via the API at one time???
+        var today = (new Date()).toISOString().split('T')[0] + 'T00:00:00Z';
+        // Make EMSSpaces API call for desired locations event reservations for today or later
         request({
-            url: 'http://lb-ems.eservices.virginia.edu/EmsPlatform/api/v1/bookings/actions/search',
+            url: 'http://lb-ems.eservices.virginia.edu/EmsPlatform/api/v1/bookings/actions/search?pageSize=2000',
             method: 'POST',
             headers: {
                 'x-ems-api-token': clientToken
             },
             body: {
                 'userBookingsOnly': false,
-                'minReserveStartTime': ,
-                'maxReserveStartTime': ,
-                'roomIds': [ ]
+                'minReserveStartTime': today,
+                'roomIds': roomIdArray
             }
         }, function(err, res) {
             // @TODO Adjust this code to accommodate the results of the EMS API calls???
             if (err) return callback(err);
             var data = JSON.parse(res.body);
-            var categories = data[0].categories;
-            // loop through categories, aka grouped spaces
-            for (var i = 0; i < categories.length; i++) {
-                var grp_name = categories[i].name.substring(0, categories[i].name.length - 1);
-                // loop through spaces to get events and generate ics file content for each
-                for (var j = 0; j < categories[i].spaces.length; j++) {
-                    var room_num = categories[i].spaces[j].name.replace(/\D/g, '');
-                    // if the room does not have a number then replace spaces in string with hyphens
-                    room_num = (room_num != '') ? room_num : categories[i].spaces[j].name.replace(/ /g, '-');
-                    var location = categories[i].spaces[j].name;
-                    var room_name = location + ' ' + grp_name;
-                    var ics_file = ical({
-                        name: room_name,
-                        prodId: { company: company_name, product: product_name },
-                        timezone: 'America/New_York',
-                        method: 'publish'
-                    });
-                    ics_file.ttl(60 * 30); //time to live of 30 minutes (same as API cache and Springshare)
-                    categories[i].spaces[j].bookings.forEach(function(evt, index, init_array) {
-                        ics_file.createEvent({ summary: evt.nickname, start: evt.start, end: evt.end, timestamp: evt.created, location: room_name });
-                    });
-                    //console.log(ics_file.toString());
-                    space_ics_files.push({ room: room_num, data: ics_file.toString() });
+            var results = data[0].results;
+            // Loop through each room ID to get those events from the results to generate a single file for each room.
+            for (var i = 0; i < roomIdArray.length; i++) {
+                var room_code = '';
+                var room_loc_num = '';
+                var building = '';
+                var timezone = '';
+                var ics_file;
+                // loop through results to retrieve one room event to capture room info and initialize iCal file
+                for (var j = 0; j < results.length; j++) {
+                    if (roomIdArray[i] == results[j].room.id) {
+                        room_code = results[j].room.description.replace(/\//g,'-').replace(/ /g,'-');
+                        room_loc_num = results[j].room.description;
+                        building = results[j].room.building.description;
+                        timezone = results[j].room.building.timeZone.name;
+                        ics_file = ical({
+                            name: building+' '+room_loc_num,
+                            prodId: { company: company_name, product: product_name },
+                            timezone: timezone,
+                            method: 'publish'
+                        });
+                        ics_file.ttl(60 * 30); //time to live of 30 minutes (same as API cache)
+                        break;
+                    }
                 } // for j
+                // loop through the results to retrieve all the events for the room to add to the iCal file
+                for (var k = 0; k < results.length; k++) {
+                    if (roomIdArray[i] == results[k].room.id) {
+                        var eventInfo = results[k].eventName + ' / ' + results[k].group.name;
+                        ics_file.createEvent({ summary: eventInfo, start: results[k].eventStartTime,
+                            end: results[k].eventEndTime, timestamp: results[k].audit.dateAdded, 
+                            location: results[k].room.description });
+                    }
+                } // for k
+                //console.log(ics_file.toString());
+                space_ics_files.push({ room: room_code, data: ics_file.toString() });
             } // for i
-            // if the API call passes the room parameter, then use the code below to
-            // retreive the desired ics data to return.
-            //space_ics_files.forEach(function(space, index, init_array) {
-            //  if (space.room == event.room) {
-            //    callback(space.data, 'Success');
-            //  }
-            //});
             // callback should return the space_ics_files dataset and then the API call using this
             // Lambda function should retrieve the appropriate room data and return it.
             callback(null, space_ics_files);
