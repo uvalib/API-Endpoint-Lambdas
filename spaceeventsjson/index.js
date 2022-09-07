7aa5719b0b31ce8ec8fad627b24da4a7905aa52a
Jack Kelly <jlk4p@virginia.edu>
2022-09-07 17:19:20 -0400

    pull in LibCal request module code

--- spaceeventsjson/index.js
+++ spaceeventsjson/index.js
@@ -1,5 +1,6 @@
 exports.handler = (event, context, callback) => {
     const axios = require('axios');
+    const request = require('request');
     const json_file = { event: [] };
     const timeOptions = { hour12: false, hour: '2-digit', minute: '2-digit' };
 
@@ -9,7 +10,13 @@ exports.handler = (event, context, callback) => {
     // 16 | CLEM407  | Clemons 407
     // 17 | CLK133   | Brown 133
     // 19 | CLEM322  | Clemons 322
-    let roomIdArray = [ 6, 16, 17, 19 ];
+    const roomIdArray = [ 6, 16, 17, 19 ];
+
+    // LibCal Space category identifiers are needed to retrieve all of the space events.
+    const spaceCategoryIDs = ['2181,8435', // brown sel
+                        '2177,11625,2197', // fine arts
+                        '3780,3781' // total advising 
+                    ];
 
     // authentication for EMS API access
     let data = { 
@@ -36,43 +43,106 @@ exports.handler = (event, context, callback) => {
             let results = data.results;
             // Loop through each room ID to get its events from the results.
             for (let i = 0; i < roomIdArray.length; i++) {
-                // loop through the results to retrieve each event's details and add to the array to be returned
-                for (let k = 0; k < results.length; k++) {
-                    if (roomIdArray[i] == results[k].room.id) {
-                        let eventInfo = results[k].eventName + ' / ' + results[k].group.name;
-                        let startDt = new Date(results[k].reserveStartTime);
-                        // using the en-CA Canadian locale for date strings to get a format of YYYY-MM-DD since US locale does not 
-                        // return that format by default.
+                // loop through the results to retrieve all the events for all EMS rooms
+                for (let j = 0; j < results.length; j++) {
+                    if (roomIdArray[i] == results[j].room.id) {
+                        let eventInfo = results[j].eventName + ' / ' + results[j].group.name;
+                        let startDt = new Date(results[j].reserveStartTime);
                         let startDate = startDt.toLocaleDateString('en-CA') + ' ' + startDt.toLocaleTimeString('en-US',timeOptions);
-                        let endDt = new Date(results[k].reserveEndTime);
+                        let endDt = new Date(results[j].reserveEndTime);
                         let endDate = endDt.toLocaleDateString('en-CA') + ' ' + endDt.toLocaleTimeString('en-US',timeOptions);
                         // for an event that ends at midnight
                         if (endDate.includes("24:00")) {
                             let endDate1 = startDt.toLocaleDateString('en-CA') + ' 23:59';
                             json_file.event.push({ name: eventInfo, startTime: startDate,
-                                endTime: endDate1, roomName: results[k].room.description, status: "confirmed" });
+                                endTime: endDate1, roomName: results[j].room.description, status: "confirmed" });
                         // for an event that runs past midnight two events need to be created as Visix doesn't support events spanning a day
                         } else if (endDate.includes("24:")) {
                             let endDate1 = startDt.toLocaleDateString('en-CA') + ' 23:59';
                             let startDate2 = endDt.toLocaleDateString('en-CA') + ' 00:00';
                             let endDate2 = endDate.replace("24:", "00:");
                             json_file.event.push({ name: eventInfo, startTime: startDate,
-                                endTime: endDate1, roomName: results[k].room.description, status: "confirmed" });
+                                endTime: endDate1, roomName: results[j].room.description, status: "confirmed" });
                             json_file.event.push({ name: eventInfo, startTime: startDate2,
-                                endTime: endDate2, roomName: results[k].room.description, status: "confirmed" });
+                                endTime: endDate2, roomName: results[j].room.description, status: "confirmed" });
                         } else {
                             json_file.event.push({ name: eventInfo, startTime: startDate,
-                                endTime: endDate, roomName: results[k].room.description, status: "confirmed" });
+                                endTime: endDate, roomName: results[j].room.description, status: "confirmed" });
                         }
                     }
-                } // for k
+                } // for j
             } // for i
-            //console.log(JSON.stringify(json_file));
-            // return JSON data for all events for all EMS and LibCal spaced referenced.
-            callback(null, json_file);
         }).catch((err) => {
             console.error(err);
         })
+        .then(function() {
+            // oAuth for LibCal API
+            request({
+                url: 'https://api2.libcal.com/1.1/oauth/token',
+                method: 'POST',
+                form: {
+                    'client_id': process.env.client_id,
+                    'client_secret': process.env.client_secret,
+                    'grant_type': 'client_credentials'
+                }
+            }, function(err, res) {
+                if (err) return callback(err);
+                // Save access token for Springshare API calls
+                let json = JSON.parse(res.body);
+                let access_token = json.access_token;
+                request({
+                    url: 'https://api2.libcal.com/1.1/space/nickname/' + spaceCategoryIDs.join(','),
+                    method: 'GET',
+                    auth: {
+                        'bearer': access_token
+                    }
+                }, function(err, res) {
+                    if (err) return callback(err);
+                    let data = JSON.parse(res.body);
+                    let categories = data[0].categories;
+                    // loop through categories, aka grouped spaces
+                    for (let i = 0; i < categories.length; i++) {
+                        // loop through spaces to get events and generate ics file content for each
+                        for (let j = 0; j < categories[i].spaces.length; j++) {
+                            let room_num = categories[i].spaces[j].name.replace(/\D/g, '');
+                            // if the room does not have a number then replace spaces in string with hyphens
+                            room_num = (room_num != '') ? room_num : categories[i].spaces[j].name.replace(/ /g, '-');                    
+                            let location = categories[i].spaces[j].name;
+                            categories[i].spaces[j].bookings.forEach(function(evt) {
+                            let startDt = new Date(evt.start);
+                            let startDate = startDt.toLocaleDateString('en-CA') + ' ' + startDt.toLocaleTimeString('en-US',timeOptions);
+                            let endDt = new Date(evt.end);
+                            let endDate = endDt.toLocaleDateString('en-CA') + ' ' + endDt.toLocaleTimeString('en-US',timeOptions);
+                            // LibCal for reserving after midnight in location that is open from mid-day overnight to next morning.
+                            if (startDate.includes("24:")) {
+                                startDate.replace("24:", "00:");
+                                if (endDate.includes("24:")) {
+                                    endDate.replace("24:","00:");
+                                }
+                                json_file.event.push({ name: evt.nickname, startTime: startDate, endTime: endDate, roomName: location, status: "confirmed" });
+                            // for an event that ends at midnight
+                            } else if (endDate.includes("24:00")) {
+                                let endDate1 = startDt.toLocaleDateString('en-CA') + ' 23:59';
+                                json_file.event.push({ name: evt.nickname, startTime: startDate, endTime: endDate1, roomName: location, status: "confirmed" });
+                            // for an event that runs past midnight two events need to be created as Visix doesn't support events spanning a day
+                            } else if (endDate.includes("24:")) {
+                                let endDate1 = startDt.toLocaleDateString('en-CA') + ' 23:59';
+                                let startDate2 = endDt.toLocaleDateString('en-CA') + ' 00:00';
+                                let endDate2 = endDate.replace("24:", "00:");
+                                json_file.event.push({ name: evt.nickname, startTime: startDate, endTime: endDate1, roomName: location, status: "confirmed" });
+                                json_file.event.push({ name: evt.nickname, startTime: startDate2, endTime: endDate2, roomName: location, status: "confirmed" });
+                            } else {
+                                json_file.event.push({ name: evt.nickname, startTime: startDate, endTime: endDate, roomName: location, status: "confirmed" });
+                            }
+                            });
+                        } // for j
+                    } // for i
+                    //console.log(JSON.stringify(json_file));
+                    // return JSON data for events for all EMS and LibCal spaced referenced.
+                    callback(null, json_file);
+                });
+            });
+        });
     }).catch((err) => {
         console.error(err);
     });
