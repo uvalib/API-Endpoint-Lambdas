b03a6a80241af0d9cd5bf4da4b62662a12afd77b
Jack Kelly <jlk4p@virginia.edu>
2024-02-23 11:11:02 -0500

    adjust to use UTC date check

--- spaceeventsjson/index.js
+++ spaceeventsjson/index.js
@@ -144,14 +144,14 @@ exports.handler = (event, context, callback) => {
                         let endDate = endDt.toLocaleDateString('en-CA') + ' ' + endDt.toLocaleTimeString('en-US',timeOptions);
                         // use nextDt field to determine if an event overlaps into the next day so we create two events.
                         let nextDt = new Date();
-                        nextDt.setDate(startDt.getUTCDate()+1);
+                        nextDt.setDate(startDt.getDate()+1);
                         // for an event that ends at midnight
                         if (endDate.includes("24:00")) {
                             let endDate1 = startDt.toLocaleDateString('en-CA') + ' 23:59';
                             json_file.event.push({ name: eventInfo, startTime: startDate,
                                 endTime: endDate1, roomName: results[j].room.description, status: "confirmed" });
                         // for an event that runs past midnight two events need to be created as Visix doesn't support events spanning a day
-                        } else if (endDate.includes("24:") || (endDt.getDate() === nextDt.getDate())) {
+                        } else if (endDate.includes("24:") || (endDt.getDate() === nextDt.getUTCDate())) {
                             let endDate1 = startDt.toLocaleDateString('en-CA') + ' 23:59';
                             let startDate2 = endDt.toLocaleDateString('en-CA') + ' 00:00';
                             let endDate2 = endDate.replace("24:", "00:");
