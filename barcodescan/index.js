8bfe0298f9223805960e98b2147bf47f1f2adc6a
Doug Chestnut <dougchestnut@gmail.com>
2024-09-13 15:53:32 -0400

    removed libraries for native node

--- barcodescan/index.js
+++ barcodescan/index.js
@@ -1,94 +1,98 @@
 exports.handler = async function(event, context, callback) {
 
-    const fetch = require('node-fetch');
-    const authURL = process.env.ilsConnectorUrl+"/v4/users/sirsi_staff_login";
-    const holdURL = process.env.ilsConnectorUrl+"/v4/requests/fill_hold/";
-    const userURL = "https://uva.hosts.atlas-sys.com/illiadwebplatform/Users/ExternalUserId/";
-    const illiadKey = process.env.ApiKey;
+  const fetch = require('node-fetch');
+  const authURL = process.env.ilsConnectorUrl+"/v4/users/sirsi_staff_login";
+  const holdURL = process.env.ilsConnectorUrl+"/v4/requests/fill_hold/";
+  const userURL = "https://uva.hosts.atlas-sys.com/illiadwebplatform/Users/ExternalUserId/";
+  const illiadKey = process.env.ApiKey;
 
-    const jsonHeaders = {
-      'Content-Type': 'application/json'
-    };
+  const jsonHeaders = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Expose-Headers': "*",
+    
+  };
 
-    const errorResponse = function(message){
-      return JSON.stringify({ "hold": {"error_messages": [ message ]} });
-    }
+  const errorResponse = function(message){
+    return JSON.stringify({ "hold": {"error_messages": [ message ]} });
+  }
 
-    const fetchUserInfo = function(holdInfo){
-      return fetch(userURL+holdInfo['user_id'], { headers:{'ApiKey':illiadKey} })
-        .then(res=>res.json())
-        .then(json=>{ return JSON.stringify({hold:holdInfo,user:json}); })
-    }
+  const fetchUserInfo = function(holdInfo){
+    return fetch(userURL+holdInfo['user_id'], { headers:{'ApiKey':illiadKey} })
+      .then(res=>res.json())
+      .then(json=>{ return JSON.stringify({hold:holdInfo,user:json}); })
+  }
 
-    const authUser = event.headers["userid"];
-    const authPass = event.headers["password"];
-    const action = event.headers["action"]; // can be set to 'auth' for auth only
-    var sessionToken = event.headers["sessiontoken"];
-    var sirsiUser;
+  const authUser = event.headers["userid"];
+  const authPass = event.headers["password"];
+  const override = event.headers["override"];
+  const action = event.headers["action"]; // can be set to 'auth' for auth only
+  var sessionToken = event.headers["sessiontoken"];
+  var sirsiUser;
 
 
-    // no authentication
-    if (!sessionToken && !authUser && !authPass)
-      callback(null, {'statusCode': 403, 'headers': jsonHeaders, 'body': errorResponse("Please provide a session or user/pass!!!")} );
-    // we have userid and pass but no token, get a session token
-    if (!sessionToken && authUser && authPass) {
-      const userpass = {username:authUser, password:authPass};
-      let response = await fetch(authURL,{ method: 'POST', headers: jsonHeaders, body: JSON.stringify(userpass)});
-      sirsiUser = await response.json();
+  // no authentication
+  if (!sessionToken && !authUser && !authPass)
+    callback(null, {'statusCode': 403, 'headers': jsonHeaders, 'body': errorResponse("Please provide a session or user/pass!!!")} );
+  // we have userid and pass but no token, get a session token
+  if (!sessionToken && authUser && authPass) {
+    const userpass = {username:authUser, password:authPass};
+console.log("body:");
+console.log(userpass);
+    let response = await fetch(authURL,{ method: 'POST', headers: jsonHeaders, body: JSON.stringify(userpass)});
+    sirsiUser = await response.json();
 console.log("sirsi User return:");
 console.log(sirsiUser);      
-      // yeah, we got a session now!
-      if (sirsiUser.sessionToken) sessionToken = sirsiUser.sessionToken;
-      // yeah, looks like those creds were bad, bummer!
-      else callback(null, {'statusCode': 401, 'headers': jsonHeaders, 'body': errorResponse("Looks like that user/pass was no good!!!")} );
-    }
+    // yeah, we got a session now!
+    if (sirsiUser.sessionToken) sessionToken = sirsiUser.sessionToken;
+    // yeah, looks like those creds were bad, bummer!
+    else callback(null, {'statusCode': 401, 'headers': jsonHeaders, 'body': errorResponse("Looks like that user/pass was no good!!!")} );
+  }
 
-    if (action === "auth" && sirsiUser) {
-      callback(null, {'statusCode': 200, 'headers': jsonHeaders, 'body': JSON.stringify(sirsiUser) } );
-    }
+  if (action === "auth" && sirsiUser) {
+    callback(null, {'statusCode': 200, 'headers': jsonHeaders, 'body': JSON.stringify(sirsiUser) } );
+  }
 
-    if (sessionToken && action !== "auth") {
-        // give the user a session token so we don't have to keep passinng that pass around
-        jsonHeaders.sessionToken = sessionToken;
-        // continue if we have a session token
-        const barcode =  (event.headers["barcode"])? event.headers["barcode"]: event['pathParameters']['barcode'];
-        if (!barcode) {
-          callback(null,
-            {
-              'statusCode': 404,
-              'headers': jsonHeaders,
-              'body': errorResponse("No barcode specified! Please try again with a barcode")
-            }
-          );
-        } else {
-          console.log("Attempt barcode api call");
-          console.log(holdURL+barcode);
-          console.log(sessionToken);
-          return fetch(holdURL+barcode,{ method: 'POST', body: '{}', headers:{'Content-Type':'application/json','Authorization':"Bearer anything",'SirsiSessionToken':sessionToken} })
-            .then(res=>res.json())
-            .then(json=>{
+  if (sessionToken && action !== "auth") {
+      // give the user a session token so we don't have to keep passinng that pass around
+      jsonHeaders.sessionToken = sessionToken;
+      // continue if we have a session token
+      const barcode =  (event.headers["barcode"])? event.headers["barcode"]: event['pathParameters']['barcode'];
+      if (!barcode) {
+        callback(null,
+          {
+            'statusCode': 404,
+            'headers': jsonHeaders,
+            'body': errorResponse("No barcode specified! Please try again with a barcode")
+          }
+        );
+      } else {
+        var barcodePath = (override=="OK")? barcode+"?override=OK":barcode
+        return fetch(holdURL+barcodePath,{ method: 'POST', body: '{}', headers:{'Content-Type':'application/json','Authorization':"Bearer anything",'SirsiSessionToken':sessionToken} })
+          .then(res=>res.json())
+          .then(json=>{
 console.log(json);
-                if ( Array.isArray(json) && json.length === 0 ) {
-                    // Invalid barcode most likely
-                    return errorResponse("It seems that you have entered an invalid barcode, please check and try again.");
-                } else {
-                    if (json["user_id"]) {
-                      // Request successful since we have a userid
-                      return fetchUserInfo(json);
-                    } else {
-                      // Most likely have some errors in the response to pass back
-                      return JSON.stringify({hold:json});
-                    }
-                }
-            })
-            .then( hold=>{
-              // send our response back through the API Gateway
-              callback(null, {'statusCode': 200, 'headers': jsonHeaders, 'body': hold} );
-            });
+              if ( Array.isArray(json) && json.length === 0 ) {
+                  // Invalid barcode most likely
+                  return errorResponse("It seems that you have entered an invalid barcode, please check and try again.");
+              } else {
+                  if (json["user_id"]) {
+                    // Request successful since we have a userid
+                    return fetchUserInfo(json);
+                  } else {
+                    // Most likely have some errors in the response to pass back
+                    return JSON.stringify({hold:json});
+                  }
+              }
+          })
+          .then( hold=>{
+            // send our response back through the API Gateway
+            callback(null, {'statusCode': 200, 'headers': jsonHeaders, 'body': hold} );
+          });
 
-        }
-    } else if (action !== "auth") {
-       callback(null, {'statusCode': 403, 'headers': jsonHeaders, 'body': errorResponse("Something odd happened, I'm not sure how you managed to get here!?!")} );
-    }
+      }
+  } else if (action !== "auth") {
+     callback(null, {'statusCode': 403, 'headers': jsonHeaders, 'body': errorResponse("Something odd happened, I'm not sure how you managed to get here!?!")} );
+  }
 
 };
--- mkrsp-quiz-3dprinter/index.js
+++ mkrsp-quiz-3dprinter/index.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-const { DateTime } = require("luxon");
 const clientDomain = process.env.libinsight_client_domain;
 const clientId = process.env.libinsight_client_id;
 const clientSecret = process.env.libinsight_client_secret;
@@ -15,16 +13,25 @@ async function getAuthToken(clientId, clientSecret) {
     });
 
     try {
-        const response = await axios.post(tokenUrl, data, {
+        const response = await fetch(tokenUrl, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
-            }
+            },
+            body: data.toString()
         });
 
-        const token = response.data.access_token;
+        if (!response.ok) {
+            const errorData = await response.text();
+            console.error('Error obtaining token:', errorData);
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        const token = responseData.access_token;
         return token;
     } catch (error) {
-        console.error('Error obtaining token:', error.response ? error.response.data : error.message);
+        console.error('Error obtaining token:', error);
         throw error; // Throw error after logging
     }
 }
@@ -39,47 +46,83 @@ async function postDataToCustomDataset(clientId, clientSecret, datasetId, datase
     const url = `https://${clientDomain}/post/v1.0/custom/${datasetId}/type/${datasetType}/save`;
 
     try {
-        const response = await axios.post(url, records, {
+        const response = await fetch(url, {
+            method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
-            }
+            },
+            body: JSON.stringify(records)
         });
 
-        return response.data;
+        if (!response.ok) {
+            const errorData = await response.text();
+            console.error('Error posting data:', errorData);
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        return responseData;
     } catch (error) {
-        console.error('Error posting data:', error.response ? error.response.data : error.message);
+        console.error('Error posting data:', error);
         throw error; // Throw error after logging
     }
 }
 
+function getCurrentTimeInTimeZone(timeZone) {
+    const date = new Date();
+    const options = {
+        timeZone,
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    };
+
+    const formatter = new Intl.DateTimeFormat('default', options);
+    const parts = formatter.formatToParts(date);
+
+    const dateParts = {};
+    for (const part of parts) {
+        dateParts[part.type] = part.value;
+    }
+
+    const formattedDate = `${dateParts.year}-${dateParts.month}-${dateParts.day} ${dateParts.hour}:${dateParts.minute}`;
+
+    return formattedDate;
+}
+
 exports.handler = async (event, context, callback) => {
-  // Make sure the form submission POST data is a JSON object.
-  const pData = event;
+    // Make sure the form submission POST data is a JSON object.
+    const pData = event;
 
-  // Verify that their field defined for the form before attempting to do anything
-  if (pData.fields.length && (pData.fields.length > 0)) {
-    // Get email address so we can strip out the computing id expected by NetBadge
-    let email = pData.fields.find(t=>t.label === "<p>Email</p>") ? pData.fields.find(t=>t.label === "<p>Email</p>").val : '';
-    if (email !== '') {
-        const parts = email.split("@"); // computing id will be parts[0]
+    // Verify that their field defined for the form before attempting to do anything
+    if (pData.fields.length && (pData.fields.length > 0)) {
+        // Get email address so we can strip out the computing id expected by NetBadge
+        let emailField = pData.fields.find(t => t.label === "<p>Email</p>");
+        let email = emailField ? emailField.val : '';
+        if (email !== '') {
+            const parts = email.split("@"); // computing id will be parts[0]
 
-        let affiliation = pData.fields.find(t=>t.label === "<p>University Affiliation</p>") ? pData.fields.find(t=>t.label === "<p>University Affiliation</p>").val : '';
-        if (affiliation !== '') {
-            const records = [
-                {
-                    ts_start: DateTime.now().setZone('America/New_York').toFormat("yyyy-LL-dd HH:mm"),
-                    field_1940: parts[0],
-                    field_1941: affiliation
+            let affiliationField = pData.fields.find(t => t.label === "<p>University Affiliation</p>");
+            let affiliation = affiliationField ? affiliationField.val : '';
+            if (affiliation !== '') {
+                const records = [
+                    {
+                        ts_start: getCurrentTimeInTimeZone('America/New_York'),
+                        field_1940: parts[0],
+                        field_1941: affiliation
+                    }
+                ];
+
+                try {
+                    await postDataToCustomDataset(clientId, clientSecret, datasetId, datasetType, records);
+                } catch (error) {
+                    console.error('Error in postDataToCustomDataset:', error);
                 }
-            ];
-            
-            try {
-                await postDataToCustomDataset(clientId, clientSecret, datasetId, datasetType, records);
-            } catch (error) {
-                console.error('Error in postDataToCustomDataset:', error);
             }
         }
     }
-  } 
-}
+};
